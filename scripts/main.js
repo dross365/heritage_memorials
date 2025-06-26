cfcc28610a1eed354546f0d38a4235a37084d579
dross365 <dross98.fullstackdev@gmail.com>
2025-06-26 10:40:23 -0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+const divElement1 = document.querySelector('#service-1-div');
+const divElement2 = document.querySelector('#service-2-div');
+const divElement3 = document.querySelector('#service-3-div');
+const divElement4 = document.querySelector('#service-4-div');
+const divElement5 = document.querySelector('#service-5-div');
+const divElement6 = document.querySelector('#service-6-div');
+const divElement7 = document.querySelector('#service-7-div');
+const divElement8 = document.querySelector('#service-8-div');
+const divElement9 = document.querySelector('#service-9-div');
+const divElement10 = document.querySelector('#service-10-div');
+const divElement11 = document.querySelector('#service-11-div');
+const divElement12 = document.querySelector('#service-12-div');
+
+divElement1.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/monuments.html'
+});
+
+divElement2.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/benches.html'
+});
+
+divElement3.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/etchings.html'
+});
+
+divElement4.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/counter-tops.html'
+});
+
+divElement5.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/cleaning.html'
+});
+
+divElement6.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/lettering.html'
+});
+
+divElement7.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/sandblasting.html'
+});
+
+divElement8.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/plowing-sanding.html'
+});
+
+divElement9.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/cemetery-restoration.html'
+});
+
+divElement10.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/rework.html'
+});
+
+divElement11.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/bronze-plaque.html'
+});
+
+divElement12.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/html/other.html'
+});
